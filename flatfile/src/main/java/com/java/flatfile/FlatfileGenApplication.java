2b4bd964d1fef53a9fd77e88856cc31f43c07932
Javaguy <131939955+Javaguy32@users.noreply.github.com>
2024-05-14 11:21:34 +0800

    Add files via upload

---
+++ flatfile/src/main/java/com/java/flatfile/FlatfileGenApplication.java
@@ -0,0 +1,13 @@
+package com.java.flatfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatfileGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatfileGenApplication.class, args);
+	}
+
+}
