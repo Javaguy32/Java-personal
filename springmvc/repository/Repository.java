5d18206d1a3696f5492a8840dd60375c1428d7e3
Javaguy <131939955+Javaguy32@users.noreply.github.com>
2024-05-14 11:32:42 +0800

    Update Repository.java

--- springmvc/repository/Repository.java
+++ springmvc/repository/Repository.java
@@ -1,1 +1,8 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroceryStoreRepository extends JpaRepository<GroceryStore, Long> {
+}
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
 
