c93425a032cd3bfc15edf257d09483513101b332
Javaguy <131939955+Javaguy32@users.noreply.github.com>
2024-05-14 11:33:48 +0800

    Update Service.java

--- springmvc/service/Service.java
+++ springmvc/service/Service.java
@@ -1,1 +1,19 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GroceryStoreService {
+
+    @Autowired
+    private GroceryStoreRepository groceryStoreRepository;
+
+}
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
+}
 
