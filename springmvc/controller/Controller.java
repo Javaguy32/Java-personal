94cf4f4000a4da10c94a7d6ebc8e8a5258dc6bec
Javaguy <131939955+Javaguy32@users.noreply.github.com>
2024-05-14 11:27:12 +0800

    Update Controller.java

--- springmvc/controller/Controller.java
+++ springmvc/controller/Controller.java
@@ -1,1 +1,25 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/grocery-stores")
+public class GroceryStoreController {
+
+    @Autowired
+    private GroceryStoreService groceryStoreService;
+
+}
+
+@RestController
+@RequestMapping("/api/restaurants")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService restaurantService;
+
+
+}
 
